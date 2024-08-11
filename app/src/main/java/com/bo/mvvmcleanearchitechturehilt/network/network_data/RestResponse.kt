1140bcabb05fddfeb8f38991921b65a131b9d694
emran <emranhossain.rip@gmail.com>
2024-08-11 10:14:16 +0600

    - code upload

--- app/src/main/java/com/bo/mvvmcleanearchitechturehilt/network/network_data/RestResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.bo.mvvmcleanearchitechturehilt.network.network_data
-
-import com.google.gson.annotations.SerializedName
-
-open class RestResponse(
-    @SerializedName("status") val status: String? = null,
-    @SerializedName("message") val message: String? = null,
-    @SerializedName("messages") val messages: List<String>? = null,
-)
